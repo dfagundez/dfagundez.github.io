68d0052a40f2c0f2683d8bddc7ecfefa3d96c639
dfglamit <dfagundez@glamit.com.ar>
2021-01-28 10:17:33 -0300

    Se carga pagina de cuenta regresiva para estreo del nuevo sitio

---
+++ js/active.js
@@ -0,0 +1,26 @@
+$(function () {
+  var countDownDate = new Date("Mar 1, 2021 10:00:00");
+  $("#countdown").countdown({
+    timestamp: countDownDate,
+    callback: function (days, hours, minutes, seconds) {
+      var message = "";
+      message += days + " day" + (days == 1 ? "" : "s") + ", ";
+      message += hours + " hour" + (hours == 1 ? "" : "s") + ", ";
+      message += minutes + " minute" + (minutes == 1 ? "" : "s") + " and ";
+      message += seconds + " second" + (seconds == 1 ? "" : "s") + " <br />";
+    },
+  });
+});
+
+var email = document.querySelector("#email");
+var btn = document.querySelector("#btn");
+
+function save_email() {
+  var get_email = email.value;
+  email.value = "";
+  if (get_email != "") {
+    swal("Good job!", "You will not miss anything :D", "success");
+  } else {
+    swal("Ups!", "Forgot to enter your email :)", "warning");
+  }
+}
